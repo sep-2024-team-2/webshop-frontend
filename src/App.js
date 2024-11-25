3ebdec98baff0da3adbae56fa8c8842914c094de
Marko Krstic <markokrstic001@gmail.com>
2024-11-25 12:43:05 +0100

    Added login form and remove card information form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserForm from "./pages/Login/Register/login";
 import HomePage from "./pages/HomePage/HomePage";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<UserForm />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Login/Register/login.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const UserForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    const url = "http://localhost:5000/api/users/login";
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Invalid email or password!");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      navigate("/home");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default UserForm;
